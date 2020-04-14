b2eaced7deb43a8aebaed6813b6ec7aae26219fe
reezafa <a.fakhreeza@gmail.com>
2020-04-15 05:38:03 +0800

    Make String from array Char

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -5,5 +5,9 @@ public class OperasiString {
         String anime = "JoJo's Bizarre Adventure";
 
         System.out.println(anime);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
     }
 }
