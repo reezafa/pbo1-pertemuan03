79f4fb7740f4a3d95791a7b9cb23a596c1f64fa4
reezafa <a.fakhreeza@gmail.com>
2020-04-15 05:32:11 +0800

    Buat Class OperasiString

---
+++ src/String/OperasiString.java
@@ -0,0 +1,9 @@
+package String;
+
+public class OperasiString {
+    public static void main(String[] args) {
+        String anime = "JoJo's Bizarre Adventure";
+
+        System.out.println(anime);
+    }
+}
