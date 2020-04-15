7492bc767dd23d6123f083f41dc042e6c1e8a90a
reezafa <a.fakhreeza@gmail.com>
2020-04-15 08:57:27 +0800

    Switch dari karakter 4 sampe akhir

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,21 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int karakter=4;
+        switch (karakter){
+            case 1:
+                System.out.println("Sorceress");
+            case 2:
+                System.out.println("Guardian");
+            case 3:
+                System.out.println("Mystic");
+            case 4:
+                System.out.println("Lahn");
+            case 5:
+                System.out.println("Berserker");
+            default:
+                System.out.println("Loli");
+        }
+    }
+}
