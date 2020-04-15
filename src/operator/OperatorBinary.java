376292a5c04f24c82674f33aab5a463fc0ac2937
reezafa <a.fakhreeza@gmail.com>
2020-04-15 08:20:02 +0800

    Operator Binary Aritmatika~

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -1,5 +1,7 @@
 package operator;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class OperatorBinary {
     // operator (=) gasan assigment/penugasan
     //menempatkan nilai kedalam variable
@@ -8,5 +10,14 @@ public class OperatorBinary {
     String nama = "Ahmad Fakhreza Faidli";
     int jumlahSks = 24;
     double indeksPresstasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(6/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 
 }
