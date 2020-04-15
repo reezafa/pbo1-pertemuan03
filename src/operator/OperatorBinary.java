8c36942403bcedd0bca29d11fccb828ce33e2ac9
reezafa <a.fakhreeza@gmail.com>
2020-04-15 08:17:04 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+    // operator (=) gasan assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //mantap dosenku :3
+
+    String nama = "Ahmad Fakhreza Faidli";
+    int jumlahSks = 24;
+    double indeksPresstasiKumulatif = 3.89;
+
+}
