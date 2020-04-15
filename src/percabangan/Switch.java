2132434a6f24198e1775d9830555ded47f41d7f9
reezafa <a.fakhreeza@gmail.com>
2020-04-15 09:00:09 +0800

    Switch untuk karakter 2 aja,bdw yang tadi ketinggalan break hihi

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,20 +2,26 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int karakter=4;
+        int karakter=2;
         switch (karakter){
             case 1:
                 System.out.println("Sorceress");
+                break;
             case 2:
                 System.out.println("Guardian");
+                break;
             case 3:
                 System.out.println("Mystic");
+                break;
             case 4:
                 System.out.println("Lahn");
+                break;
             case 5:
                 System.out.println("Berserker");
+                break;
             default:
                 System.out.println("Loli");
+                break;
         }
     }
 }
