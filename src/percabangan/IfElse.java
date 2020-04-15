9d153f272e38db36ac4d4f80b8876d9f1b162be7
reezafa <a.fakhreeza@gmail.com>
2020-04-15 08:45:06 +0800

    ifelse true diatas false di bawah

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double  pembayaran= 103500;
+        double  adaduit= 100000;
+
+        if (adaduit<pembayaran){
+            System.out.println("Duitnya kurang nah, kurangi dulu dikit");
+        }else{
+            System.out.println("Duitnya cukup, Selamat menikmati dan jangan jera");
+        }
+    }
+}
