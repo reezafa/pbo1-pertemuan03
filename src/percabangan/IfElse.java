c16294f24fc4a4b2b7161ae153da1821b1f2676c
reezafa <a.fakhreeza@gmail.com>
2020-04-15 08:52:50 +0800

    ifelse versi3

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,17 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double  pembayaran= 93500;
+        double  pembayaran= 73500;
         double  adaduit= 100000;
 
         if (adaduit<pembayaran){
             System.out.println("Duitnya kurang nah, kurangi dulu dikit");
-        }else{
-            System.out.println("Duitnya cukup, Berelaan cil, jangan jera");
+        }else if(adaduit>pembayaran){
+            double  kembalian=adaduit-pembayaran;
+            System.out.println("kembalian : "+kembalian);
+        }
+        else{
+            System.out.println("Duitnya pas jual lah, Berelaan cil, jangan jera");
         }
     }
 }
